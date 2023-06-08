fa9ace0d212ac4bc4ef89e11d6133948788e46a5
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-08 14:07:54 -0600

    Initial Commit

---
+++ SwiftUIConcurrencyST/SwiftUIConcurrencySTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConcurrencySTApp.swift
+//  SwiftUIConcurrencyST
+//
+//  Created by Mitch Andrade on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConcurrencySTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
